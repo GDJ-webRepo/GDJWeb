b639988565cfbefe52c3a5a4d1e14c6330c9fd9d
juva <juvet.quentin19@gmail.com>
2022-11-16 23:06:28 +0100

    fix:merge develop

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -3,6 +3,7 @@ import { FormControl, FormGroup, NonNullableFormBuilder } from '@angular/forms';
 import { HotToastService } from '@ngneat/hot-toast';
 import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
 import { switchMap, tap } from 'rxjs';
+import { AuthService } from 'src/app/shared/services/auth/auth.service';
 
 
 
@@ -14,26 +15,10 @@ import { switchMap, tap } from 'rxjs';
   styleUrls: ['./profile.component.scss'],
 })
 export class ProfileComponent implements OnInit {
-  // user$ = this.usersService.currentUserProfile$;
-
-  profileForm = this.fb.group({
-    uid: [''],
-    displayName: [''],
-    firstName: [''],
-    lastName: [''],
-    phone: [''],
-    address: [''],
-  });
-
   constructor(
-
-    private toast: HotToastService,
-    private fb: NonNullableFormBuilder
-  ) {}
-
-  ngOnInit(): void {
-  
-  }
+    public authService: AuthService
+  ) { }
+  ngOnInit() { }
 
 
   // saveProfile() {
