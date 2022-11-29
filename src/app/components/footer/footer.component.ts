ac71bdac31d2cb8d922491723d3bf18899170b7c
Célian Frasca <celian.frasca@gmail.com>
2022-11-29 13:53:04 +0100

    feat: Add Formulaire Mini Backend

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+  styleUrls: ['./footer.component.scss'],
 })
 export class FooterComponent implements OnInit {
+  userName!: string;
+  userEmail!: string;
+  userMessage!: string;
 
-  constructor() { }
+  constructor(private router: Router) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  onSubmitForm(form: NgForm) {
+    console.log(form.value);
   }
-
 }
